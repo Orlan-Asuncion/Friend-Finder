e0856e1218d275e52588c2460494716a51141320
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-05-30 13:44:09 -0400

    update friends.js and checked/modified codes apiRoutes.js

--- data/friends.js
+++ data/friends.js
@@ -1,38 +1,38 @@
 
 
-var friendsArray = [
+var friends = [
     {
-        name: "Jim O'brien",
-        photo: "img",
+        name: "Annalie Delos Reyes",
+        photo: "https://media.licdn.com/dms/image/C5603AQEiRZO03-ou-w/profile-displayphoto-shrink_800_800/0?e=1564617600&v=beta&t=aUcNeOBfDC1DzxbeM-9T4D4LcUGtkJTqD9yNplxv6tc",
         score: [5, 4, 3, 6,7,3,2,2,1,6]
     },
    
     {
         name: " Victor Hoang",
-        photo: "img",
+        photo: "https://media.licdn.com/dms/image/C5603AQG1wev0e4w7tg/profile-displayphoto-shrink_800_800/0?e=1564012800&v=beta&t=Kl5ixiRp8jKUxwNWP9SWDbdmb4kmeV7Y0KtmVvhBXQU",
         score: [5, 4, 3, 6,7,3,2,2,1,6]  
     },
     {
-        name: "Jonathan Chang",
-        photo: "img",
+        name: "Billy Cheng",
+        photo: "https://media.licdn.com/dms/image/C5603AQHeP38oZJQ54w/profile-displayphoto-shrink_800_800/0?e=1564012800&v=beta&t=yDxsQU3ZwWG4Q8ScB7sXgkKG5fhqtGkF1N7xjCUnTRU",
         score: [1, 4, 3, 4, 7, 3, 2, 2, 1,6]  
     },
     {
-        name: "Anthony Perez",
-        photo: "img",
+        name: "Trueman Pang",
+        photo: "https://media.licdn.com/dms/image/C4D03AQHtM8q_URpSVg/profile-displayphoto-shrink_800_800/0?e=1564617600&v=beta&t=2c4BNvm5pwlszBU9awsX2D9JV-lq1NIMggjBVvIh9VIhttps://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-1/p112x112/47400373_2583900595178316_6990943672430755840_n.jpg?_nc_cat=106&_nc_ht=scontent.fmia1-1.fna&oh=33562220398a2e6dec8fd28bd2b1019b&oe=5D926DD3",
         score: [5, 4, 3, 6,7,3,2,2,1,6]  
     },
     {
-        name: "Julio Diaz",
-        photo: "img",
+        name: "Mike Wang",
+        photo: "https://media.licdn.com/dms/image/C5603AQE9r0oIPmBN-Q/profile-displayphoto-shrink_800_800/0?e=1564617600&v=beta&t=n_y7mkr9EqOrak7m6-qPGnHI_bq9YkL10QzpnY0m3-s",
         score: [5, 4, 3, 6,7,3,2,2,1,6]  
     },
     {
-        name: "Monica Herrera",
-        photo: "img",
+        name: "Stephanie Soliven",
+        photo: "https://media.licdn.com/dms/image/C5103AQG5xe0piE06Rg/profile-displayphoto-shrink_800_800/0?e=1564012800&v=beta&t=cPLPb0WGbkwvyBWd6DUYHbv0sJAu1J5IIg0rReB8GqE",
         score: [5, 4, 3, 6,7,3,2,2,1,6]  
     },
 
-]
+];
 
-module.exports = friendsArray;
+module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,23 +1,48 @@
 var friends = require("../data/friends.js");
 
-module.exports = function(app){
-    app.get("/api/friends", function(req, res){
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-    app.post("/api/friends", function(req, res){
-        var totalDiff = 0;
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+    app.post("/api/friends", function (req, res) {
+       
         var userData = req.body;
         console.log(userData);
         var userName = userData.name;
         var userScores = userData.scores;
 
-        var b = userScores.map(function(item){
+        var b = userScores.map(function (item) {
             return parseInt(item, 10);
         });
-    });
-};
+        
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        };
+        console.log(userData);
+        console.log("Name: " + userName);
+        console.log("User score " + userScores);
+
+        var leastDiff  = 41;
+        var index = 0;
+        for (var i = 0; i < friends.length; i++) {
+            var currentDiff = 0;
+            for(var j = 0;j < friends[0].score.length; j++){
+                currentDiff = currentDiff + Math.abs(friends[i].score[j] - userData.scores[j]); 
+            }
+            if( currentDiff < leastDiff){
+                leastDiff = currentDiff;
+                index = i;
+            }
+
+        }
+           var bestMatch = friends[index];
+            console.log(bestMatch);
+            friends.push(userData);
+            console.log("New User added");
+            console.log(userData);
+            res.json(bestMatch);
+        
+        }); 
+    };
